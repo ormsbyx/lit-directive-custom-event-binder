79b744a9946317b0ead779aa7f193c93e3021028
Chris Ormsby <ormsbyx@pm.me>
2023-03-11 10:27:10 -0500

    Updated README and script comments

--- README.md
+++ README.md
@@ -1,37 +1,36 @@
 # lit-directive-custom-event-binder
 
-For use with Lit web-components library, version 2 (+).
+For use with the Lit web components library, version 2(+).
 
 This is Directive for the [Lit web comoponent library](https://lit.dev/). It will bind an event listener to the element that it is used on. It takes two arguments: the event name, and a function.
 
-I created this to be able to bind events with non-standard names. The `@event` binding that
-you can use in Lit `render()` functions can not handle event-names with dashes or non alpha
-characters. This solves that problem.
+This allows one to bind events with non-standard names to elements in a LitElement. The `@eventname` binding that you can use in Lit `render()` functions can not handle event-names with dashes or non alpha characters. This directive solves that problem.
 
-The callback function should expect the usual Event argument, but can also ignore it.
-(e.g. it can be defined to accept no arguments)
+The callback function will receive the standard event argument that any callback bound with `addEventListener()` would get.
 
-Example usage:
-(your imports, at the top of your script file):
+### Example usage:
+Import `getCustomEventBinder`.
 
 ```
 import { getCustomEventBinder } from "/path/to/file/ci-lit-directive-custom-event-binder.ts";
-import { MY_FUNKY_EVENT } from "/path/to/file/my-custom-events.ts";
+import { MY_FUNKY_EVENT } from "/path/to/file/my-custom-events.ts"; // value is my-funky-event
 ```
 
-(in your class definition, or elsewhere):
+Instantiate the directive at the top of your file (outside your class definition). Of course you could make it a class property too.
 
 ```
 const myEventBinder = getCustomEventBinder();
 ```
 
-(in your render() method -- or some method that renders with Lit's html`` template-literal tag)
+Then, in some method that renders with an ` html`` ` template-literal tag, you use it the way you would use other Lit directives (like `ref()`, for example):
 
 ```
-return html`
-      <div
-        class="someClass"
-        ${this.myEventBinder(MY_FUNKY_EVENT, this.handleFunkyEvent.bind(this))}
-      >Some Text</div>
-`;
+render() {
+  return html`
+    <div
+      class="someClass"
+      ${myEventBinder(MY_FUNKY_EVENT, this.handleFunkyEvent.bind(this))}
+    >Some Text</div>
+  `;
+}
 ```
--- src/lit-directive-custom-event-binder.ts
+++ src/lit-directive-custom-event-binder.ts
@@ -4,26 +4,42 @@ import { Directive, directive, ElementPart } from "lit/directive.js";
 type GenericEventListenerCallback = (evt?: Event) => void;
 
 /**
- * For use with Lit web-components library.
- * This is a Lit Directive which will add an event listener to the element that it is used on.
- * Requires two arguments: an event name, and a properly bound event callback function.
- * This callback function should expect the usual Event argument, but can also ignore it.
- * (e.g. it can be defined to accept no arguments)
+ * For use with the Lit web components library, version 2(+).
+ *
+ * This is Directive for the Lit web comoponent library. It will bind an event listener to
+ * the element that it is used on. It takes two arguments: the event name, and a function.
+ *
+ * This allows one to bind events with non-standard names to elements in a LitElement.
+ * The @eventname binding that you can use in Lit render() functions can not handle
+ * event-names with dashes or non alpha characters. This directive solves that problem.
+ *
+ * The callback function will receive the standard event argument that any callback bound
+ * with addEventListener() would get.
+ *
  * Example usage:
- *  (your imports, at the top of your script file) -->
- *      import { getCustomEventBinder } from "/path/to/file/ci-lit-directive-custom-event-binder.ts";
- *      import { MY_FUNKY_EVENT } from "/path/to/file/my-custom-events.ts";
- *  (in your class definition, or elsewhere) -->
- *      const myEventBinder = getCustomEventBinder();
- *  (in your render() method -- or some method that renders with Lit's html`` template-literal tag)
- *      return html`
- *           <div 
-    *            class="someClass"
-                 ${this.myEventBinder(MY_FUNKY_EVENT, this.handleFunkyEvent.bind(this))}
-             >
- *           Some Text</div>
- *      `;
- * 
+ * Import getCustomEventBinder.
+ *
+ * import { getCustomEventBinder } from "/path/to/file/ci-lit-directive-custom-event-binder.ts";
+ * import { MY_FUNKY_EVENT } from "/path/to/file/my-custom-events.ts"; // value is my-funky-event
+ *
+ * Instantiate the directive at the top of your file (outside your class definition). Of course
+ * ou could make it a class property too.
+ *
+ * const myEventBinder = getCustomEventBinder();
+ *
+ * Then, in some method that renders with an html`` template-literal tag, you use it the way
+ * you would use other Lit directives (like ref(), for example):
+ *
+
+ *   render() {
+ *     return html`
+ *       <div
+ *         class="someClass"
+ *         ${myEventBinder(MY_FUNKY_EVENT, this.handleFunkyEvent.bind(this))}
+ *       >Some Text</div>
+ *       `;
+ *     }
+ *
  * @param {string} eventName - The name of any valid html event or any custom defined event
  * @param {string} callback - An event callback (function). can be defined with 0 or 1 argument.
  */
@@ -42,7 +58,7 @@ export class CustomEventFunctionBinder extends Directive {
   }
 
   /**
-    * This returns a Lit Directive which will add an event listener to the element 
+    * This returns a Lit Directive which will add an event listener to the element
     * that it is used on. See class definition for usage example.
    * @returns function
    */
